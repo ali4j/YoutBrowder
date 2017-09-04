27b984d104c68314107767b6385c84041266c7a4
ali4j <rashidialimail@gmail.com>
2017-09-04 21:24:38 +0330

    gets jquery source from its website and loads it by webengine  executes a script to changes all of the visible links in loaded document  adds new interface and moves all constants into it

---
+++ src/main/java/net/ali4j/youtbrowder/Constants.java
@@ -0,0 +1,11 @@
+package net.ali4j.youtbrowder;
+
+/**
+ * Created by ehsan on 9/4/2017.
+ */
+public interface Constants {
+    String DEFAULT_URL = "https://youtube.com";
+    String JQUERY_LOCATION = "https://code.jquery.com/jquery-3.2.1.min.js";
+    String SCRIPT = "$(\"a:visible\").each(function() {$(this).css(\"text-transform\", \"uppercase\");});";
+
+}
--- src/main/java/net/ali4j/youtbrowder/YoutBrowder.java
+++ src/main/java/net/ali4j/youtbrowder/YoutBrowder.java
@@ -3,14 +3,15 @@ package net.ali4j.youtbrowder;
 /**
  * Created by ehsan on 8/29/2017.
  */
+
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -23,18 +24,21 @@ import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.events.Event;
 import org.w3c.dom.events.EventListener;
 import org.w3c.dom.events.EventTarget;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 /**
- * A sample that demonstrates a WebView object accessing a web page.
- * Most of the codes are taken form the oracle samples
+ *
+ * Some of the codes are taken from the oracle samples, SO and other websites
  */
 public class YoutBrowder extends Application {
-
-    public static final String DEFAULT_URL = "https://youtube.com";
 
     private static final Logger logger = Logger.getLogger(YoutBrowder.class);
 
@@ -57,17 +61,14 @@ public class YoutBrowder extends Application {
         }
     }
 
-
-
-
     public Parent createContent() {
 
         WebView webView = new WebView();
 
         final WebEngine webEngine = webView.getEngine();
-        webEngine.load(DEFAULT_URL);
+        webEngine.load(Constants.DEFAULT_URL);
 
-        final TextField locationField = new TextField(DEFAULT_URL);
+        final TextField locationField = new TextField(Constants.DEFAULT_URL);
         webEngine.locationProperty().addListener(
                 (ObservableValue<? extends String> observable, String oldValue, String newValue) ->
                 {
@@ -75,35 +76,24 @@ public class YoutBrowder extends Application {
         });
 
 
-
-        webEngine.on
-        webEngine.documentProperty().addListener((observable1, oldValue1, newValue1) -> {
-            logger.debug("newValue:" + newValue1);
-        });
-
-        webEngine.getLoadWorker().property
-
         webEngine.getLoadWorker().stateProperty().addListener(
             (observable, oldValue, newValue) -> {
-
-                if(newValue== Worker.State.SCHEDULED){
-                    System.out.println("state: scheduled");
-                } else if(newValue== Worker.State.RUNNING){
-                    System.out.println("state: running");
-                } else if(newValue== Worker.State.SUCCEEDED){
-                    System.out.println("state: succeeded");
+                if( newValue != Worker.State.SUCCEEDED ) return;
+                try{
+                    loadJquery(webEngine);
+                }catch(IOException ioe){
+                    logger.error(ioe);
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error Dialog");
+                    alert.setHeaderText("Exception happened");
+                    alert.setContentText("please check you internet connection");
+                    alert.showAndWait();
+                    alert.setOnCloseRequest(e -> System.exit(1));
                 }
 
-
-                if( newValue != Worker.State.SUCCEEDED ) return;
-
-                logger.debug("webengine is loaded");
-
+                logger.debug("web engine is loaded");
                 goButton.setDisable(false);
                 stopButton.setDisable(true);
-
-
-
 
                 EventListener listener = ev -> {
                     String href = ((Element)ev.getTarget()).getAttribute("href");
@@ -115,14 +105,12 @@ public class YoutBrowder extends Application {
                 if(logger.isDebugEnabled()) logger.debug("href list size:" + hrefList.getLength());
                 for (int i=0; i<hrefList.getLength(); i++)
                     ((EventTarget)hrefList.item(i)).addEventListener("click", listener, false);
-
             }
         );
 
 
         EventHandler<ActionEvent> goAction = e -> {
             logger.debug("loading address");
-
             goButton.setDisable(true);
             stopButton.setDisable(false);
             webEngine.load(locationField.getText().startsWith("http://")
@@ -133,7 +121,6 @@ public class YoutBrowder extends Application {
 
         EventHandler<ActionEvent> optionsAction = e ->{
             logger.debug("options is clicked");
-
             if(optionsDialog==null)
                 optionsDialog = new OptionsDialog();
             else
@@ -177,6 +164,37 @@ public class YoutBrowder extends Application {
         VBox.setVgrow(webView, Priority.ALWAYS);
         return vBox;
     }
+
+
+
+    public void loadJquery(WebEngine webEngine) throws IOException {
+        StringBuilder jQueryContents = new StringBuilder();
+        BufferedReader in = null;
+        URLConnection urlConnection;
+        URL jqueryLocation;
+        try {
+            jqueryLocation = new URL(Constants.JQUERY_LOCATION);
+            urlConnection = jqueryLocation.openConnection();
+            urlConnection.connect();
+            in = new BufferedReader(new InputStreamReader(jqueryLocation.openStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                jQueryContents.append(inputLine);
+            webEngine.executeScript(jQueryContents.toString());
+            logger.trace("jquery is loaded and executed");
+            webEngine.executeScript(Constants.SCRIPT);
+            logger.trace("script executed");
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
